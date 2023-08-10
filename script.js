15bd741cfcf59e7aeda36722756f25a056189a5a
runasin <nacikgoz4745@gmail.com>
2023-08-11 01:59:14 +0300

    nw

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = ()=> {
+    menuIcon.classList.toggle('ri-close-fill');
+    menuIcon.classList.toggle('active');
+};
+
+
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
